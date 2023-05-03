356a3ceea8629406eb30db731f124eebda45edb5
VirLg <19virlg@gmail.com>
2023-05-03 23:48:25 +0300

    markup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Feedback from './components/state/state'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Feedback />
+    </div>
+  );
+}
+
+export default App;
